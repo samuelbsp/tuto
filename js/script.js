f89ee81fd5833324cc107a6562fee9de47fdcd6c
samuelbsp <sambolduc@gmail.com>
2015-02-13 12:44:12 -0500

    Second Commit

---
+++ js/script.js
@@ -0,0 +1,9 @@
+codifyMarkup(".code");
+function codifyMarkup(selector) {
+  var classSel = document.querySelectorAll(selector);
+  for(var i = 0; i < classSel.length; i++) {
+    var currentText = classSel[i].innerHTML;
+    var fixedText = currentText.replace(/</g, "&lt;").replace(/>/g, "&gt;");
+    classSel[i].innerHTML = fixedText;
+  }
+}
